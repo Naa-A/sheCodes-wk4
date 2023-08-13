a23ff27f17f6dba3d7da0b8fd3b29aacde5c2de2
Naa-A <61292016+Naa-A@users.noreply.github.com>
2023-08-12 20:12:54 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import "./styles.css";
+import Weather from "./Weather.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
